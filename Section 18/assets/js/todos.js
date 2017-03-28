2aa61ed4e4015238857d6ef1aed26203f09e9661
Icaro Aguiar Perez <icaro.1508@gmail.com>
2017-03-28 14:15:15 -0300

    Finished To-Do List Project

--- Section 18/assets/js/todos.js
+++ Section 18/assets/js/todos.js
@@ -3,6 +3,8 @@ $("ul").on("click", "li", function () {
     $(this).toggleClass("completed");
 });
 
+
+//Removes To-Dos from the To-Do List - Deletes them after FadeOut
 $("ul").on("click", "span", function (event) {
     $(this).parent().fadeOut(500, function () {
         $(this).remove();
@@ -10,6 +12,7 @@ $("ul").on("click", "span", function (event) {
     event.stopPropagation();
 });
 
+//Adds items to To-Do List with input box pressing enter
 $("input[type='text']").keypress(function (event) {
     if (event.which === 13) {
         //grabbing new todo text from input
@@ -18,4 +21,10 @@ $("input[type='text']").keypress(function (event) {
         //create new li and add to ul
         $("ul").append("<li><span><i class='fa fa-minus'></i></span> " + todoText + "</li>")
     }
+});
+
+$(".fa-arrow-circle-down").click(function () {
+    $("#toggle-input-box-div").slideToggle();
+    $(this).toggleClass("fa-plus");
+    $(this).toggleClass("fa-arrow-circle-down");
 });
