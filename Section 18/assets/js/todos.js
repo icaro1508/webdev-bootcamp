085e4c510e3d238971bf4d9040b502b0c8d00c1c
Icaro Aguiar Perez <432398@celg.go>
2017-03-28 10:38:57 -0300

    Lecture 183 - Finished To-Do List functionality

--- Section 18/assets/js/todos.js
+++ Section 18/assets/js/todos.js
@@ -1,10 +1,21 @@
-$("li").click(function(){
+//check off specific todos by clicking
+$("ul").on("click", "li", function () {
     $(this).toggleClass("completed");
 });
 
-$("span").click(function(event){
-   $(this).parent().fadeOut(500, function(){
-       $(this).remove();
-   });
-    event.stopPropagation();   
+$("ul").on("click", "span", function (event) {
+    $(this).parent().fadeOut(500, function () {
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function (event) {
+    if (event.which === 13) {
+        //grabbing new todo text from input
+        var todoText = $(this).val();
+        $(this).val("");
+        //create new li and add to ul
+        $("ul").append("<li><span>X</span> " + todoText + "</li>")
+    }
 });
